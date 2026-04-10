7116883062c9b0c690a3278ed412e507d538c98d
Manju <bhargava.1314@gmail.com>
2026-04-10 18:08:37 +0000

    c prog added

---
+++ C_prog.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    int a = 10, b = 20, sum;
+
+    sum = a + b;
+
+    printf("Hello, GitHub!\n");
+    printf("Sum of %d and %d is: %d\n", a, b, sum);
+
+    return 0;
+}
